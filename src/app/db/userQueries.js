1edb96a2023f78c7e84c01e3fb367eb450f42ce2
haimeo1201 <hvhai87@gmail.com>
2022-12-20 08:17:35 +0700

    123

--- src/app/db/userQueries.js
+++ src/app/db/userQueries.js
@@ -52,6 +52,9 @@ class userQueries {
                   },
                 },
               },
+              author: {
+                select: { name: true },
+              },
               sharedFrom: true,
             },
           },
