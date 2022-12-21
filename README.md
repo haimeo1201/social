956ec32c1d11877698505d2c06d5cd1e5f2b1c6e
Tuan Minh Ngo <catboom01@gmail.com>
2022-12-21 23:17:32 +0700

    docs

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# social
-back-end for project
+# Socii
+
+A Social-Networking Application, built with React and ExpressJS. Database is managed with Prisma and MySQL.
+
+This is a group project for INT3306 22 class - Web Application Development.
+
+## Team Members
+
+| Name                   | Student ID |
+| --------------         | ---------- |
+| Nguyen Kien Thai Duong | 20020043   |
+| Hoang Viet Hai         | 20020049   |
+| Mai Tuan Nghia         | 20020262   |
+| Ngo Tuan Minh          | 20020059   |
+
+# Functionality
+
+## Installation
+
+First, clone the repositories to this folder structure:
+
+[socii-be](https://github.com/haimeo1201/socii-be) <br>
+├── *(repo's file)* <br>
+[socii-fe](https://github.com/duongdaydungso/socii-fe) <br>
+├── build <br>
+├── *(repo's file)* <br>
+
+Then, adjust the `REACT_APP_BACKEND_URL` environment variable in `socii-fe/.env` to the backend's host's URL (default is [localhost:8080](http://localhost:8080)) and `FRONTEND_URL` in `socii-be/.env` to the frontend's host's URL (default is [localhost:3000](http://localhost:3000)).
+
+In production, both of these URL should be the domain name of the deployment server.
+
+Next, install the dependencies for both the backend and the frontend:
+
+```bash
+cd socii-be
+npm i
+cd ../socii-fe
+npm i
+```
+
+Next, compile a production build of the frontend:
+
+```bash
+cd socii-fe
+npm run build
+```
+
+Then, initialize the database with Prisma:
+
+```bash
+cd socii-be
+npm run initdb
+```
+
+Finally, start the server from backend:
+
+```bash
+cd socii-be
+npm run dev
+```
+
+Website should be available at [localhost:8080](http://localhost:8080).
--- src/index.js
+++ src/index.js
@@ -3,22 +3,56 @@ const app = express();
 const cors = require("cors");
 const path = require("path");
 const routes = require("./routes/index");
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT;
-const frontendPort = process.env.FRONTEND_URL;
-app.use("/image", express.static("./public/image"));
-app.use("/video", express.static("./public/video"));
-app.use(
-  cors({
-    origin: [frontendPort],
-    credentials: true,
-  })
-);
+const frontendURL = process.env.FRONTEND_URL;
 
 app.use(express.json({ limit: "50mb" }));
 
+app.use("/image", express.static("./public/image"));
+app.use("/video", express.static("./public/video"));
+
+app.use(express.static("../socii-fe/build"));
+app.get("/post/*", function (req, res) {
+    res.sendFile(
+        path.join(__dirname, "../../socii-fe/build/index.html"),
+        function (err) {
+            if (err) {
+                res.status(500).send(err);
+            }
+        }
+    );
+});
+app.get("/profile/*", function (req, res) {
+    res.sendFile(
+        path.join(__dirname, "../../socii-fe/build/index.html"),
+        function (err) {
+            if (err) {
+                res.status(500).send(err);
+            }
+        }
+    );
+});
+app.get("/friendlist", function (req, res) {
+    res.sendFile(
+        path.join(__dirname, "../../socii-fe/build/index.html"),
+        function (err) {
+            if (err) {
+                res.status(500).send(err);
+            }
+        }
+    );
+});
+
+app.use(
+    cors({
+        origin: [frontendURL],
+        credentials: true,
+    })
+);
 routes(app);
 
 app.listen(port, () => {
-  console.log("listen at port " + port);
+    console.log("listen at port " + port);
 });
